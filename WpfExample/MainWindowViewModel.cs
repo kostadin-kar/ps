6a7d38b4882613c0d5c4cd4f7552d2edb0aff1c4
kostadin-kar <koce.kar@gmail.com>
2020-04-11 21:51:10 +0300

    Task 5: MVVM - no bonus tasks

---
+++ WpfExample/MainWindowViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfExample
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private ICommand hiButtonCommand;
+        private ICommand toggleExecuteCommand
+        { get; set; }
+        private bool canExecute = true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string HiButtonContent
+        {
+            get { return "click to hi"; }
+        }
+
+        public string GreetContent
+        {
+            get; set;
+        }
+
+        public bool CanExecute
+        {
+            get { return this.canExecute; }
+            set
+            {
+                if (this.canExecute == value)
+                {
+                    return;
+                }
+                this.canExecute = value;
+            }
+        }
+
+        public ICommand ToggleExecuteCommand
+        {
+            get
+            { return toggleExecuteCommand; }
+            set
+            { toggleExecuteCommand = value; }
+        }
+
+        public ICommand HiButtonCommand
+        {
+            get
+            { return hiButtonCommand; }
+            set
+            { hiButtonCommand = value; }
+        }
+
+        public MainWindowViewModel()
+        {
+            HiButtonCommand = new RelayCommand(ShowMessage, param => this.canExecute);
+            toggleExecuteCommand = new RelayCommand(ChangeCanExecute);
+        }
+
+        public void ShowMessage(object obj)
+        {
+            //it is good we do this with binding to some control  
+            //System.Windows.MessageBox.Show(obj.ToString());
+            GreetContent = string.Format("{0} - {1}", obj.ToString(), DateTime.Now.ToLongDateString());
+            OnPropertyChanged("GreetContent");
+        }
+
+        public void ChangeCanExecute(object obj)
+        {
+            canExecute = !canExecute;
+        }
+    }
+}
